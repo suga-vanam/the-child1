4a5ddf108c9113347b0ef8e721aaa9baac0f0c63
suga-vanam <71540414+suga-vanam@users.noreply.github.com>
2020-10-01 17:30:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const prefix = '-';
 client.once('ready', () => {
     console.log('among us is online!');
     client.user.setActivity('among us');
+
 });
 
 client.on('message' , message => {
@@ -22,17 +23,27 @@ client.on('message' , message => {
     if(command === 'ping'){
         message.channel.send('pong!');
     }
-    if(command ==='thala'){
+    if(command === 'thala'){
         message.channel.send('lebron is da GOAT!https://www.youtube.com/watch?v=L2evguInh5Y');
+    }
+    if(command === 'thala'){
+        message.channel.send(':goat:');
     }
     if(command === 'help'){
         message.channel.send(`
         these are my supported commands:
         **-ping**:to get a pong
         **-thala**:to see who the GOAT is
+        **-quote**:to see the wisdom of FIITJEE
         **-me**:to mute everyone
         **-ue**:to unmute everyone
         `);
+    }
+
+    if(command === 'quote'){
+        var quotes = ["sometimes my squares are a bit round-CVR","Studying is a non-spontaneous process. Watching TV isn't-CVR","Am I hot-BJS?","Don't do hard work, don't do smart work, just do work-MRJ","if you go minus 1, I go minus 2-BJS","Just bother yourself!!-PCM"]
+        var quote = Math.floor(Math.random() * quotes.length);
+        message.channel.send(quotes[quote])
     }
     if(command === 'me'){
         if (message.member.voice.channel)
