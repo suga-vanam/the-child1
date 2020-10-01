ec95d77ed16e2e307c624b284f464d802c61013d
suga-vanam <71540414+suga-vanam@users.noreply.github.com>
2020-10-01 20:24:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ client.on('message' , message => {
     }
 
     if(command === 'quote'){
-        var quotes = ["sometimes my squares are a bit round-CVR","Studying is a non-spontaneous process. Watching TV isn't-CVR","Am I hot-BJS?","Don't do hard work, don't do smart work, just do work-MRJ","if you go minus 1, I go minus 2-BJS","Just bother yourself!!-PCM","Getting into IIT is easy,getting ur girlfriend into IIT is tough-CVR","pls no one listen to my instructions-EAB","You dont want to wash your face like you wash your clothes-CVR","No, that is the centre of the earth, you cannot go there-PGR","Just like real life, even raolut's law is not perfect-CVR", "i can read most of your minds-EAB","CORONA BANDEMIC-EAB","I like Ph-CVR"]
+        var quotes = ["sometimes my squares are a bit round-CVR","Studying is a non-spontaneous process. Watching TV isn't-CVR","Am I hot?-BJS","Don't do hard work, don't do smart work, just do work-MRJ","if you go minus 1, I go minus 2-BJS","Just bother yourself!!-PCM","Getting into IIT is easy,getting ur girlfriend into IIT is tough-CVR","pls no one listen to my instructions-EAB","You dont want to wash your face like you wash your clothes-CVR","No, that is the centre of the earth, you cannot go there-PGR","Just like real life, even raolut's law is not perfect-CVR", "i can read most of your minds-EAB","CORONA BANDEMIC-EAB","I like Ph-CVR"]
         var quote = Math.floor(Math.random() * quotes.length);
         message.channel.send(quotes[quote])
     }
